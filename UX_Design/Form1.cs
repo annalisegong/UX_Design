d871b340c07e2433b0ba13f8a00002bfa5534dab
annalisegong <annalise.gong@cuw.edu>
2022-09-19 10:39:09 -0500

    update UI
    added labels, buttons, and textboxes for the compute MP page

--- UX_Design/Form1.cs
+++ UX_Design/Form1.cs
@@ -6,5 +6,35 @@ namespace UX_Design
 		{
 			InitializeComponent();
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void flowLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
